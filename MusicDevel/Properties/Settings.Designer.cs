daf27019ed9857d46723471cf38449f12ad0f91e
phil8822 <zartan@marvista.com>
2026-05-17 21:53:44 -0700

    Change SQL server to MV7

--- MusicDevel/Properties/Settings.Designer.cs
+++ MusicDevel/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MusicDevel.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace MusicDevel.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MV5;Initial Catalog=MyDemo;User ID=sa;Password=sasa")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MV7;Initial Catalog=MyDemo;User ID=sa;Password=sasa")]
         public string CnxStringHome {
             get {
                 return ((string)(this["CnxStringHome"]));
